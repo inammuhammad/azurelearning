4e6adc14a804106eec9d8d9c4baea886cda6a28e
muhammad.inamuh <muhammad.inamuh@gmail.com>
2022-07-20 18:35:39 +0500

    minor change

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World 2!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
