f739e1ab2cd2063d1d1dddaeab2025aef064c7f2
muhammad.inamuh <muhammad.inamuh@gmail.com>
2022-07-21 15:54:36 +0500

    User Module - List - added dummy list of users

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,9 +1,23 @@
 import {Request, Response} from 'express';
+import { IUser } from '../../models/IUser';
 import { SuccessResponse, ResponseStatusString } from '../../utils/app-response';
 
 export class UserController {
     public static async List(req: Request, res: Response) {
-        const response = new SuccessResponse(res, ResponseStatusString.SUCCESS, 'List fetch success');
+        const users: IUser[] = [{
+            email: 'user1@domain.com',
+            id: 1,
+            name: 'User 1'
+        }, {
+            email: 'user2@domain.com',
+            id: 2,
+            name: 'User 2'
+        }, {
+            email: 'user3@domain.com',
+            id: 3,
+            name: 'User 3'
+        }];
+        const response = new SuccessResponse(res, ResponseStatusString.SUCCESS, users);
         response.send();
     }
 }
